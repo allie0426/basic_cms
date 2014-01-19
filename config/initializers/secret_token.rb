87240d5810c6fcfba528b11738eb3539d947b586
Allie Tyre <shopaddy@gmail.com>
2014-01-19 13:40:03 -0500

    Initial commit of basic_cms

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BasicCms::Application.config.secret_key_base = '733a5131f1b6bf4027a1ceab6254bdb0b82a7ad61ff1c4ab7eaa122bf36bbc6e377f1c67c18ee133e063d44e0f61eae7f7bdba8703e90cbf7a6bd30edf42487c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BasicCms::Application.config.session_store :cookie_store, key: '_basic_cms_session'
